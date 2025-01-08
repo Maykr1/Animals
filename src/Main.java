0d9ea85b6620cc77c364e88e8d34b3073cdac8a3
Maykr1 <eclark72713@gmail.com>
2025-01-08 14:34:20 -0500

    Finished

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ package Animals.src;
 
 public class Main {
     public static void main(String[] args) {
+        Dog dog = new Dog("Buddy", 3);
+        Cat cat = new Cat("Whiskers", 2);
+
+        dog.eat();
+        dog.sleep();
+        dog.bark();
+
+        System.out.println("\n");
         
+        cat.eat();
+        cat.sleep();
+        cat.meow();
     }
 }
