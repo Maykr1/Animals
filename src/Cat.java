8d7799eb598ebfad4e44438525f5ef21332e7b85
Maykr1 <eclark72713@gmail.com>
2025-01-08 14:29:21 -0500

    Created Subclasses Cat and Dog

--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,16 @@
 package Animals.src;
 
-public class Cat {
-    
+public class Cat extends Animal {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    public void meow() {
+        System.out.println(getName() + " meowed!");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating cat food.");
+    }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,5 +1,16 @@
 package Animals.src;
 
-public class Dog {
-    
+public class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    public void bark() {
+        System.out.println(getName() + " barked!");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating dog food.");
+    }
 }
